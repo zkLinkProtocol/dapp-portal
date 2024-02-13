ceb96f4f65223ff2ec35427951e7ce113e24354c
Dev <dev@zklink.org>
2024-02-13 14:12:35 +0800

    feat: export zkSyncProvider:requestPrimaryProvider

--- store/zksync/provider.ts
+++ store/zksync/provider.ts
@@ -1,22 +1,45 @@
 import { Provider } from "@/zksync-web3-nova/src";
 
 import { useNetworkStore } from "@/store/network";
+import useNetworks from "@/composables/useNetworks";
 
 export const useZkSyncProviderStore = defineStore("zkSyncProvider", () => {
   const { selectedNetwork } = storeToRefs(useNetworkStore());
+  const { primaryNetwork } = useNetworks();
   let provider: Provider | undefined;
-
+  let primaryProvider: Provider | undefined;
   const requestProvider = () => {
+    const eraNetwork = selectedNetwork.value;
     if (!provider) {
-      provider = new Provider(selectedNetwork.value.rpcUrl);
+      provider = new Provider(eraNetwork.rpcUrl);
     }
+    //if provider.networkKey != eraNetwork.key
+    console.log(eraNetwork.key);
+    provider.setContractAddresses(eraNetwork.key, {
+      mainContract: eraNetwork.mainContract,
+      erc20BridgeL1: eraNetwork.erc20BridgeL1,
+      erc20BridgeL2: eraNetwork.erc20BridgeL2,
+    });
     return provider;
+  };
+
+  const requestPrimaryProvider = () => {
+    if (!primaryProvider) {
+      primaryProvider = new Provider(primaryNetwork.rpcUrl);
+    }
+    primaryProvider.setContractAddresses(primaryNetwork.key, {
+      mainContract: primaryNetwork.mainContract,
+      erc20BridgeL1: primaryNetwork.erc20BridgeL1,
+      erc20BridgeL2: primaryNetwork.erc20BridgeL2,
+    });
+    return primaryProvider;
   };
 
   return {
     eraNetwork: selectedNetwork,
 
     requestProvider,
+    requestPrimaryProvider,
 
     blockExplorerUrl: computed(() => selectedNetwork.value.blockExplorerUrl),
   };
