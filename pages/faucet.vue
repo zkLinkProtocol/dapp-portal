f1ec85203efed71825b3447164be45a902e932db
sarah <sarah@zklink.org>
2024-02-28 20:34:30 +0800

    fix:change faucet description style

--- pages/faucet.vue
+++ pages/faucet.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <PageTitle>Faucet</PageTitle>
     <TypographyCategoryLabel>You can get Goerli Testnet ETH from any of the following faucets.</TypographyCategoryLabel>
     <CommonCardWithLineButtons>
-      <DestinationItem v-for="(item, index) in links" :key="index" v-bind="item"> </DestinationItem>
+      <DestinationItem v-for="(item, index) in links" :key="index" v-bind="item" :ellipsisdescription="true">
+      </DestinationItem>
     </CommonCardWithLineButtons>
     <TypographyCategoryLabel>
       If you’re from an established project and need more testnet ETH, please reach out to us through telegram:
@@ -15,8 +17,8 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/vue/24/outline";
 const links = [
   {
     iconUrl: "/img/chainstack-faucet-logo.png",
-    label: "ETHEREUM GOERLI FAUCET",
-    description: "",
+    label: "Ethereum Goerli Faucet",
+    description: "A fast and reliable Ethereum Goerli testnet faucet for blockchain developers.",
     as: "a",
     href: "https://www.alchemy.com/faucets/ethereum-goerli",
     target: "_blank",
@@ -24,7 +26,8 @@ const links = [
   {
     iconUrl: "/img/EthereumGoerli-Faucet-logo.png",
     label: "Ethereum Faucet for Goerli by QuickNode",
-    description: "",
+    description:
+      "Easy to use Ethereum Faucet for Goerli. Claim your testnet tokens for free - one drip per network every 12 hours.",
     as: "a",
     href: "https://faucet.quicknode.com/ethereum/goerli",
     target: "_blank",
@@ -32,10 +35,12 @@ const links = [
   {
     iconUrl: "/img/Goerli-faucet-logo.svg",
     label: "Goerli Faucet - Get Free Goerli Testnet ETH",
-    description: "",
+    description:
+      "Get free Goerli testnet tokens from Chainstack's faucet. Sign up and get free Goerli testnet ETH tokens.",
     as: "a",
     href: "https://faucet.chainstack.com/goerli-testnet-faucet",
     target: "_blank",
   },
 ];
 </script>
+<style scoped></style>
