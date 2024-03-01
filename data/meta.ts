47abf1ca0dd85fdfe124d2d6f4fc6a11b85c3ed5
sarah <sarah@zklink.org>
2024-03-01 11:27:15 +0800

    fix:change  Open Graph image

--- data/meta.ts
+++ data/meta.ts
@@ -3,7 +3,7 @@ export const portal = {
   description:
     "zkLink Nova Portal allows you to view your balances, transfer tokens and bridge assets between zkLink Nova and Ethereum",
   previewImg: {
-    src: "../public/img/cion.png",
+    src: "/img/preview.png",
     alt: "zkLink Nova Portal",
   },
 };
@@ -12,7 +12,7 @@ export const bridge = {
   description:
     "With the zkLink Nova Bridge you can easily deposit tokens to zkLink Nova. Enjoy faster, cheaper and more efficient transactions with the future proof zkEVM scaling Ethereum's security and values.",
   previewImg: {
-    src: "../public/img/cion.png",
+    src: "/img/preview.png",
     alt: "zkLink Nova Bridge",
   },
 };
