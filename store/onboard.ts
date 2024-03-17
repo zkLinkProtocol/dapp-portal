ec315ae3acc24e0dda02ca6dad2c7a290eaebe9c
Evan <evan@zklink.org>
2024-03-17 13:56:10 +0800

    fix: change logo onboard.ts

--- store/onboard.ts
+++ store/onboard.ts
@@ -73,7 +73,7 @@ export const useOnboardStore = defineStore("onboard", () => {
     name: "zkLink Nova Portal",
     description: "zkLink Nova Portal - view balances, transfer and bridge tokens",
     url: "https://portal.zklink.io",
-    icons: ["../public/img/icon.png"],
+    icons: ["../public/img/favicon.png"],
   };
   const wagmiConfig = createConfig({
     autoConnect: true,
@@ -124,7 +124,7 @@ export const useOnboardStore = defineStore("onboard", () => {
     excludeWalletIds: ["bc949c5d968ae81310268bf9193f9c9fb7bb4e1283e1284af8f2bd4992535fd6"],
     featuredWalletIds: [
       // "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",rainbow
-      "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // okx wallet 
+      // "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // okx wallet 
       "8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4", // binance web3 wallet
       "c7708575a2c3c9e6a8ab493d56cdcc56748f03956051d021b8cd8d697d9a3fd2", // fox wallet
       // "38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662",//bitget
