d7b404c9f15aa9d2bf51bd9c7b2be2329e0e44e7
MickWang <1244134672@qq.com>
2024-04-03 22:57:58 +0800

    fix check supported walelts

--- data/wallets.ts
+++ data/wallets.ts
@@ -1,22 +1,27 @@
-type WalletEntry = { walletName: string; type: "injected" | "metaMask" | "walletConnect" | "safe"};
+type WalletEntry = { walletName: string; type: "injected" | "metaMask" | "walletConnect" | "safe" };
 
 export const confirmedSupportedWallets: WalletEntry[] = [
   { walletName: "MetaMask", type: "metaMask" },
 
+  { walletName: "OKX", type: "injected" },
+  { walletName: "GateWallet", type: "injected" },
   { walletName: "MetaMask", type: "injected" },
   { walletName: "BitKeep", type: "injected" },
   { walletName: "BlockWallet", type: "injected" },
   { walletName: "MathWallet", type: "injected" },
   { walletName: "ImToken", type: "injected" },
-  
+
+  { walletName: "GateWallet", type: "walletConnect" },
   { walletName: "MetaMask", type: "walletConnect" },
   { walletName: "imToken", type: "walletConnect" },
   { walletName: "OKX", type: "walletConnect" },
+
+  { walletName: "Binance DeFi", type: "walletConnect" },
   { walletName: "Binance Web3", type: "walletConnect" },
   { walletName: "Bitget", type: "walletConnect" },
   { walletName: "Trust", type: "walletConnect" },
   { walletName: "TokenPocket", type: "walletConnect" },
-  { walletName: "Safe", type: "safe" }
+  { walletName: "Safe", type: "safe" },
 ];
 
 export const disabledWallets: WalletEntry[] = [
