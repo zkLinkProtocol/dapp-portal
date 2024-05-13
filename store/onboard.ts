dc5f92bdd9ebebef22781ebbc5b313598761b39f
MickWang <1244134672@qq.com>
2024-05-13 22:44:27 +0800

    add domain

--- store/onboard.ts
+++ store/onboard.ts
@@ -87,7 +87,7 @@ export const useOnboardStore = defineStore("onboard", () => {
   const connectors = [
     injected(),
     safe({
-      allowedDomains: [/app.safe.global$/],
+      allowedDomains: [/app.safe.global$/, /safe.zklink.io$/],
       debug: true,
     }),
     walletConnect({ projectId: env.walletConnectProjectID, showQrModal: false, metadata }),
