01fe653be5e8734a068dc7dbb4e0b2c14e269b6a
MickWang <1244134672@qq.com>
2024-06-21 14:05:36 +0800

    fix ui issuees

--- utils/mappers.ts
+++ utils/mappers.ts
@@ -5,46 +5,43 @@ import type { Ref } from "vue";
 
 export const groupBalancesByAmount = <T = TokenAmount>(balances: Ref<T[]>, networkKey?: string) =>
   computed(() => {
-    const groups: Record<string, { title: string | null; balances: T[] }> = {
-      default: {
-        title: null,
-        balances: [],
-      },
-      small: {
-        title: "Small balances",
-        balances: [],
-      },
-      zero: {
-        title: "Zero balances",
-        balances: [],
-      },
-    };
-    for (const balanceItem of balances.value as (T & TokenAmount)[]) {
-      const decimalBalance =
-        typeof balanceItem.price === "number"
-          ? removeSmallAmount(balanceItem.amount, balanceItem.decimals, balanceItem.price)
-          : parseTokenAmount(balanceItem.amount, balanceItem.decimals);
-      if (!isOnlyZeroes(decimalBalance)) {
-        groups.default.balances.push(balanceItem);
-      } else if (decimalBalance === "0") {
-        groups.zero.balances.push(balanceItem);
-      } else {
-        groups.small.balances.push(balanceItem);
-      }
-    }
-    const res = [groups.default, groups.small, groups.zero].filter((group) => group.balances.length);
-    if (networkKey === "ALL") {
-      const result = { title: null, balances: [] as TokenAmount[] };
-      for (const group of res) {
-        result.balances.push(...(group.balances as TokenAmount[]));
-      }
-      result.balances.forEach(
-        (item) => (item.usdBalance = Number(formatRawTokenPrice(item.amount, item.decimals, item.price ?? 0)))
-      );
-      result.balances.sort((a, b) => (b.usdBalance ?? 0) - (a.usdBalance ?? 0));
-      return [result];
-    }
-    return res;
+    // const groups: Record<string, { title: string | null; balances: T[] }> = {
+    //   default: {
+    //     title: null,
+    //     balances: [],
+    //   },
+    //   small: {
+    //     title: "Small balances",
+    //     balances: [],
+    //   },
+    //   zero: {
+    //     title: "Zero balances",
+    //     balances: [],
+    //   },
+    // };
+    // for (const balanceItem of balances.value as (T & TokenAmount)[]) {
+    //   const decimalBalance =
+    //     typeof balanceItem.price === "number"
+    //       ? removeSmallAmount(balanceItem.amount, balanceItem.decimals, balanceItem.price)
+    //       : parseTokenAmount(balanceItem.amount, balanceItem.decimals);
+    //   if (!isOnlyZeroes(decimalBalance)) {
+    //     groups.default.balances.push(balanceItem);
+    //   } else if (decimalBalance === "0") {
+    //     groups.zero.balances.push(balanceItem);
+    //   } else {
+    //     groups.small.balances.push(balanceItem);
+    //   }
+    // }
+    // const res = [groups.default, groups.small, groups.zero].filter((group) => group.balances.length);
+    // return res;
+
+    const balanceWithUsd = (balances.value as (T & TokenAmount)[]).map((item) => ({
+      ...item,
+      usdBalance: Number(formatRawTokenPrice(item.amount, item.decimals, item.price ?? 0)),
+    })) as TokenAmount[];
+
+    balanceWithUsd.sort((a, b) => (b.usdBalance ?? 0) - (a.usdBalance ?? 0));
+    return [{ title: null, balances: balanceWithUsd }];
   });
 
 export const mapApiToken = (token: Api.Response.Token): Token => {
